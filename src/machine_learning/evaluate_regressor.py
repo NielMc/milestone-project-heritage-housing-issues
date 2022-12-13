c1c460030360c4d3ca56e8c7d4c572ca3686c1c7
JessMair <jessiegrewal11@gmail.com>
2022-12-13 12:26:14 +0000

    Add code for the evaluat_ regressor.py file to display R2 scores and plots

--- src/machine_learning/evaluate_regressor.py
+++ src/machine_learning/evaluate_regressor.py
@@ -1,27 +1,45 @@
-import streamlit as st
-import pandas as pd
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error 
+import numpy as np
 
-# code copied from "Modeling and Evaluation" notebooks
-def confusion_matrix_and_report(X,y,pipeline,label_map):
+"""
+Results of the regression evaluation
+"""
+def regression_performance(X_train, y_train, X_test, y_test,pipeline):
+	st,write("Model Evaluation \n")
+	st.write("* Train Set")
+	regression_evaluation(X_train,y_train,pipeline)
+	st.write("* Test Set")
+	regression_evaluation(X_test,y_test,pipeline)
 
+"""
+Code to returns R2 score for both test and train sets for pipeline
+"""
+def regression_evaluation(X,y,pipeline):
   prediction = pipeline.predict(X)
+  st.write('R2 Score:', r2_score(y, prediction).round(3))  
+  st.write('Mean Absolute Error:', mean_absolute_error(y, prediction).round(3))  
+  st.write('Mean Squared Error:', mean_squared_error(y, prediction).round(3))  
+  st.write('Root Mean Squared Error:', np.sqrt(mean_squared_error(y, prediction)).round(3))
+  st.write("\n")
 
-  st.write('###  Confusion Matrix ')
-  st.code(pd.DataFrame(confusion_matrix(y_true=prediction, y_pred=y),
-        columns=[ ["Actual " + sub for sub in label_map] ], 
-        index= [ ["Prediction " + sub for sub in label_map ]]
-        ))
-("\n")
+"""
+Plots to evaluate regression
+"""
+def regression_evaluation_plots(X_train, y_train, X_test, y_test,pipeline, alpha_scatter=0.5):
+  pred_train = pipeline.predict(X_train)
+  pred_test = pipeline.predict(X_test)
 
+  fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12,6))
+  sns.scatterplot(x=y_train , y=pred_train, alpha=alpha_scatter, ax=axes[0])
+  sns.lineplot(x=y_train , y=y_train, color='red', ax=axes[0])
+  axes[0].set_xlabel("Actual")
+  axes[0].set_ylabel("Predictions")
+  axes[0].set_title("Train Set")
 
-  st.write('### Classification Report')
-  st.code(classification_report(y, prediction, target_names=label_map),"\n")
+  sns.scatterplot(x=y_test , y=pred_test, alpha=alpha_scatter, ax=axes[1])
+  sns.lineplot(x=y_test , y=y_test, color='red', ax=axes[1])
+  axes[1].set_xlabel("Actual")
+  axes[1].set_ylabel("Predictions")
+  axes[1].set_title("Test Set")
 
-
-def regressor_performance(X_train,y_train,X_test,y_test,pipeline,label_map):
-  print("#### Train Set #### \n")
-  confusion_matrix_and_report(X_train,y_train,pipeline,label_map)
-
-  print("#### Test Set ####\n")
-  confusion_matrix_and_report(X_test,y_test,pipeline,label_map)
+  st.pyplot.show()
