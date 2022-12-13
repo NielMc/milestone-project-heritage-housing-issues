94f7447ef9ed0e7691e7601af285a6a7885dced8
JessMair <jessiegrewal11@gmail.com>
2022-12-13 16:34:12 +0000

    Add code to sale_price_study

--- app_pages/page_sale_price_study.py
+++ app_pages/page_sale_price_study.py
@@ -1,6 +1,98 @@
 import streamlit as st
+from src.data_management import load_house_data
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set_style("whitegrid")
+from feature_engine.discretisation import ArbitraryDiscretiser
+import numpy as np
+import plotly.express as px
 
 def study ():
 
-     st.write("### something")
+    # load data
+    df = load_house_data()
 
+    # hard copied from churned customer study notebook
+    vars_to_study = ['OverallQual', 'GrLivArea', 'YearBuilt', 'TotalBsmtSF', 'GarageArea']
+
+    st.write("### House Price Study")
+    st.info(
+        f"* The client is interested in discovering the patterns from the dataset pertaining to house sale values. "
+        f"This is so that they are aware of the most relevant variables correlated "
+        f"to a the sale price. This will be demonstrated via the use of data visualisations. "
+        )
+
+
+    # inspect data
+    if st.checkbox("Inspect House Dataset"):
+        st.write(
+            f"* The dataset has {df.shape[0]} rows and {df.shape[1]} columns, "
+            f"find below the first 10 rows.")
+        
+        st.write(df.head(10))
+
+    st.write("---")
+
+
+    # Correlation Study Summary
+    st.write(
+        f"* A correlation study was conducted in the notebook to better understand how "
+        f"the variables are correlated to the 'SalePrice'. \n"
+        f"The most correlated variable are: **{vars_to_study}**"
+    )
+
+    # Text based on "02 - Churned Customer Study" notebook - "Conclusions and Next steps" section
+    st.info(
+        f""
+    )
+
+
+ # Individual plots per variable
+    if st.checkbox("Churn Levels per Variable"):
+        churn_level_per_variable(df_eda)
+        
+    # Parallel plot
+    if st.checkbox("Parallel Plot"):
+        st.write(f"* Information in yellow indicates the profile from a churned customer")
+        parallel_plot_churn(df_eda)
+
+
+# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
+def plot_categorical(df, col, target_var):
+    fig, axes = plt.subplots(figsize=(12, 5))
+    sns.countplot(data=df, x=col, hue=target_var,order = df[col].value_counts().index)
+    plt.xticks(rotation=90) 
+    plt.title(f"{col}", fontsize=20,y=1.05)        
+    st.pyplot(fig) # st.pyplot() renders image, in notebook is plt.show()
+
+
+# code copied from "02 - Churned Customer Study" notebook - "Variables Distribution by Churn" section
+def plot_numerical(df, col, target_var):
+    fig, axes = plt.subplots(figsize=(8, 5))
+    sns.histplot(data=df, x=col, hue=target_var, kde=True,element="step") 
+    plt.title(f"{col}", fontsize=20,y=1.05)
+    st.pyplot(fig) # st.pyplot() renders image, in notebook is plt.show()
+
+
+
+# function created using "02 - Churned Customer Study" notebook code - Parallel Plot section
+def parallel_plot_churn(df_eda):
+
+    tenure_map = [-np.Inf, 6, 12, 18, 24, np.Inf] # hard coded from "disc.binner_dict_['tenure']"" result,
+                                                  # found at "02 - Churned Customer Study" notebook
+                                                  # under "Parallel Plot" section
+    disc = ArbitraryDiscretiser(binning_dict={'tenure': tenure_map})
+    df_parallel = disc.fit_transform(df_eda)
+    
+    n_classes = len(tenure_map) - 1
+    classes_ranges = disc.binner_dict_['tenure'][1:-1]
+    LabelsMap = {}
+    for n in range(0,n_classes):
+        if n == 0: LabelsMap[n] = f"<{classes_ranges[0]}"
+        elif n == n_classes-1: LabelsMap[n] = f"+{classes_ranges[-1]}"
+        else: LabelsMap[n] = f"{classes_ranges[n-1]} to {classes_ranges[n]}"
+
+
+    df_parallel['tenure'] = df_parallel['tenure'].replace(LabelsMap)
+    fig = px.parallel_categories(df_parallel, color="Churn", width=750, height=500)
+    st.plotly_chart(fig)  # we use st.plotly_chart() to render, in notebook is fig.show()
