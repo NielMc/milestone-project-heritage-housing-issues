462122a24d583d31deef03aa854f69b451d31397
JessMair <jessiegrewal11@gmail.com>
2022-12-19 01:29:39 +0000

    Add target distribution plot to study app page

--- app_pages/multi_pages.py
+++ app_pages/multi_pages.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+        
 # Class to generate multiple Streamlit pages using an object oriented approach 
 class MultiPage: 
 
@@ -7,12 +7,7 @@ class MultiPage:
         self.pages = []
         self.app_name = app_name
 
-        st.set_page_config(
-            page_title=self.app_name,
-            page_icon="🖥️") # You may add an icon, to personalize your App
-        # check links below for additional icons reference
-        # https://docs.streamlit.io/en/stable/api.html#streamlit.set_page_config
-        # https://twemoji.maxcdn.com/2/test/preview.html
+        
     
     def add_page(self, title, func) -> None: 
         self.pages.append({"title": title, "function": func })
