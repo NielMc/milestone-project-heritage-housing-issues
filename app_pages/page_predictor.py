36c9dbc76d63037311180d1c676550a30ae6a254
JessMair <jessiegrewal11@gmail.com>
2022-12-20 13:28:41 +0000

    Debug code that is effecting the output of the widgets

--- app_pages/page_predictor.py
+++ app_pages/page_predictor.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import pandas as pd
 from src.data_management import load_house_data, load_pkl_file, load_inherited_house_data
-# from src.machine_learning.predictive_analysis_ui import predict_sale_price
-# from datetime import date
+from src.machine_learning.predictive_analysis_ui import predict_sale_price
+from datetime import date
 
 def p5_widgets():
     #load required files
     version = 'v1'
-    df_inherited = load_inherited_house_data()
     regressor_pipeline = load_pkl_file(f"outputs/ml_pipeline/predict_saleprice/{version}/regressor_pipeline.pkl")
     house_features = (pd.read_csv(f"outputs/ml_pipeline/predict_saleprice/{version}/X_train.csv")
                             .columns
@@ -28,10 +27,14 @@ def p5_widgets():
     st.write("We can use the toggles below to input and amend data to gain insights in the "
     "estimated house price. Please note, the values are set at a default of median value")
 
-    # Generate Live Data
+# Generate Live Data
+
+    X_live = DrawInputsWidgets()
+
+# Predict on live data
 
     if st.button("Run Predictive Analysis"):
-        predict_sale_price(DrawInputsWidgets(), house_features, sale_price_pipeline)
+        predict_sale_price(X_live, house_features, sale_price_pipeline)
 
     st.write("---")
     st.write("Below we have the information the client requires regarding the sale price of "
@@ -53,76 +56,137 @@ def p5_widgets():
 
 def DrawInputsWidgets():
 
-# load dataset
-  df = load_house_data()
-  percentageMin, percentageMax = 0.4, 2.0
+	# Load the dataset
+	df = load_house_data()
+	percentageMin, percentageMax = 0.4, 2.0
 
-# We create input widgets only for 6 features
-  col1, col2, col3 = st.beta_columns(2)
-  col4, col5 = st.beta_columns(2)
+    # Create input widgets for the most important features	
+	col1, col2 = st.beta_columns(2)
+	col3, col4, col5 = st.beta_columns(3)
 
-# We are using these features to feed the ML pipeline
-# Create an empty DataFrame, which will be the live data
+	# Feed the ML pipeline by using these features
+		
+	# Create an empty DataFrame, to be displayed in the live data
+	X_live = pd.DataFrame([], index=[0]) 
+	
+	# Then draw the widget based on the numerical or categorical type
+	# and set initial values
 
-  X_live = pd.DataFrame([], index=[0])
-    
-  """from here on we draw the widget based on the variable type (numerical or categorical)
-  # and set initial values"""
+	with col1:
+		feature = "OverallQual"
+		st_widget = st.number_input(
+			label= feature,
+			min_value= 0, 
+			max_value= 10,
+			value= df[feature].median(),        
+			)
+	X_live[feature] = st_widget
 
-with col1:
-    feature = "OverallQual"
-    st_widget = st.number_input(
-        label= feature,
-        min_value= 0,
-        max_value= 10,
-        value= df[feature].median()
-        )
+	with col2:
+		feature = "GrLivArea"
+		st_widget = st.number_input(
+			label= feature,
+			min_value= df[feature].min()*percentageMin, 
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median(), 
+			)
+	X_live[feature] = st_widget
 
-(DrawInputsWidgets())[feature] = st_widget
+    with col3:
+		feature = "YearBuilt"
+		st_widget = st.number_input(
+			label= feature,
+			min_value= df[feature].min()*percentageMin, 
+			max_value= date.today().year,
+			value= df[feature].median(), 
+			)
+	X_live[feature] = st_widget
 
-with col2:
-    feature = "GrLivArea"
-    st_widget = st.number_input(
-        label= feature,
-        min_value= df[feature].min()*percentageMin,
-        max_value= df[feature].max()*percentageMax,
-        value= int(df[feature].median())
-        )
+    with col4:
+		feature = "GarageArea"
+		st_widget = st.number_input(
+			label= feature,
+			min_value= df[feature].min()*percentageMin, 
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median(), 
+			)
+	X_live[feature] = st_widget
 
-(DrawInputsWidgets())[feature] = st_widget
+	with col5:
+		feature = "TotalBsmtSF"
+		st_widget = st.number_input(
+			label= feature,
+			min_value= df[feature].min()*percentageMin, 
+			max_value= df[feature].max()*percentageMax,
+			value= df[feature].median(), 
+			)
+	X_live[feature] = st_widget
 
-with col3:
-  feature = "YearBuilt"
-  st_widget = st.number_input(
-    label= feature,
-    min_value= df[feature].min()*percentageMin,
-    max_value= df[feature].max()*percentageMax,
-    value= df[feature].median()
-    )
 
-(DrawInputsWidgets())[feature] = st_widget
+	# st.write(X_live)
 
-with col4:
-  feature = "TotalBsmtSF"
-  st_widget = st.number_input(
-    label= feature,
-    min_value= df[feature].min()*percentageMin,
-    max_value= df[feature].max()*percentageMax,
-    value= df[feature].median()
-    )
+	return X_live
+                    
 
-(DrawInputsWidgets())[feature] = st_widget
 
-with col5:
-  st_widget = st.number_input(
-  label= feature,
-    min_value= df[feature].min()*percentageMin,
-    max_value= df[feature].max()*percentageMax,
-    value= df[feature].median()
-    )
-    
-(DrawInputsWidgets())[feature] = st_widget
 
-# st.write(X_live)
+# """from here on we draw the widget based on the variable type (numerical or categorical) and set initial values"""
 
-#return X_live
+# with col1:
+#     feature = "OverallQual"
+#     st_widget = st.number_input(
+#         label= feature,
+#         min_value= 0,
+#         max_value= 10,
+#         value= df[feature].median()
+#         )
+
+# X_live[feature] = st_widget
+
+# with col2:
+#     feature = "GrLivArea"
+#     st_widget = st.number_input(
+#         label= feature,
+#         min_value= df[feature].min()*percentageMin,
+#         max_value= df[feature].max()*percentageMax,
+#         value= int(df[feature].median())
+#         )
+
+# X_live[feature] = st_widget
+
+# with col3:
+#     feature = "YearBuilt"
+#     st_widget = st.number_input(
+#         label= feature,
+#         min_value= df[feature].min()*percentageMin,
+#         max_value= df[feature].max()*percentageMax,
+#         value= df[feature].median()
+#         )
+
+# X_live[feature] = st_widget
+
+# with col4:
+#     feature = "TotalBsmtSF"
+#     st_widget = st.number_input(
+#         label= feature,
+#         min_value= df[feature].min()*percentageMin,
+#         max_value= df[feature].max()*percentageMax,
+#         value= df[feature].median()
+#         )
+
+# X_live[feature] = st_widget
+
+# with col5:
+#     feature = "GarageArea"
+#     st_widget = st.number_input(
+#         label= feature,
+#         min_value= df[feature].min()*percentageMin,
+#         max_value= df[feature].max()*percentageMax,
+#         value= df[feature].median()
+#         )
+
+#     X_live[feature] = st_widget
+
+#     st.write(X_live)
+
+#     return X_live
